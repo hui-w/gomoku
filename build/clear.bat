ae68e17ad4ae5bfd8ffabb8e4fc27f04ee5d1355
hui-w <huiwang@hpe.com>
2017-02-09 16:52:13 +0800

    Add files for build and release

---
+++ build/clear.bat
@@ -0,0 +1,4 @@
+SET TOOLS=%~DP0
+SET SRC=%TOOLS%\..\src
+SET OUT=%TOOLS%\..\output
+rd /S /Q %OUT%
---
+++ build/minify.bat
@@ -0,0 +1,35 @@
+@ECHO ON
+REM Starting to minify JS & CSS files ...
+
+@ECHO OFF
+REM don't watch the sausage being made
+
+REM the folder
+SET TOOLS=%~DP0
+SET SRC=%TOOLS%\..\src
+SET RES=%TOOLS%\..\res
+SET OUT=%TOOLS%\..\output
+
+rd /S /Q %OUT%
+md "%OUT%\"
+
+REM Combine JS & CSS files into one file
+type "%SRC%\app.js" "%SRC%\common.js" "%SRC%\config.js" "%SRC%\game.js" > "%OUT%\temp.app.js"
+type "%SRC%\web.css" > "%OUT%\temp.app.css"
+
+REM Compress with YUI Compressor  
+java -jar "%TOOLS%\yuicompressor-2.4.7.jar" -o "%OUT%\app.min.js" "%OUT%\temp.app.js"
+java -jar "%TOOLS%\yuicompressor-2.4.7.jar" -o "%OUT%\app.min.css" "%OUT%\temp.app.css"
+
+REM Delete temporary files
+del "%OUT%\temp.app.js"
+del "%OUT%\temp.app.css"
+
+REM Index Files
+copy "%SRC%\index_release.html" "%OUT%\index.html"
+
+REM Copy other files
+copy "%RES%\favicon.ico" "%OUT%\favicon.ico"
+copy "%RES%\apple-touch-icon.png" "%OUT%\apple-touch-icon.png"
+
+@ECHO ON
