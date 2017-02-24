e8eca977959c42cf7ca8b700ceb39c7976356c6c
hui-w <huiwang@qlike.com>
2017-02-24 17:47:17 +0800

    Update home buttons with dynamic position

--- src/home.js
+++ src/home.js
@@ -7,38 +7,53 @@
  */
 var home_prototype = {
   onHide: null,
+  btnWidth: null,
+  btnHeight: null,
+  btn1: null,
+  btn2: null,
+  btn3: null,
 
   init: function(left, top) {
     this._super(left, top);
 
     // Init the value
     this.onHide = null;
+    this.btnWidth = 150;
+    this.btnHeight = 32;
 
     this.onRenderExtra.push(this.renderMask);
 
-    var btn1 = new Button(100, 50, 150, 32, 'button1');
-    btn1.setText('Human vs. Human');
-    btn1.onClick = function() {
+    this.btn1 = new Button(0, 0, 150, 32, 'button1');
+    this.btn1.setText('Human vs. Human');
+    this.btn1.onClick = function() {
       console.log('Button1 clicked');
       this.triggerOnHide();
     }.bind(this);
-    this.addChild(btn1);
+    this.addChild(this.btn1);
 
-    var btn2 = new Button(100, 100, 150, 32, 'button2');
-    btn2.setText('Human vs. Robot');
-    btn2.onClick = function() {
+    this.btn2 = new Button(0, 0, 150, 32, 'button2');
+    this.btn2.setText('Human vs. Robot');
+    this.btn2.onClick = function() {
       console.log('Button2 clicked');
       this.triggerOnHide();
     }.bind(this);
-    this.addChild(btn2);
+    this.addChild(this.btn2);
 
-    var btn3 = new Button(100, 150, 150, 32, 'button3');
-    btn3.setText('Robot vs. Human');
-    btn3.onClick = function() {
+    this.btn3 = new Button(0, 0, 150, 32, 'button3');
+    this.btn3.setText('Robot vs. Human');
+    this.btn3.onClick = function() {
       console.log('Button3 clicked');
       this.triggerOnHide();
     }.bind(this);
-    this.addChild(btn3);
+    this.addChild(this.btn3);
+
+    this.onSizeChanged = this.sizeChangedHandler;
+  },
+
+  sizeChangedHandler: function(width, height) {
+    this.btn1.setPosition((width - this.btnWidth) / 2, height / 2 - 1.5 * this.btnHeight);
+    this.btn2.setPosition((width - this.btnWidth) / 2, height / 2);
+    this.btn3.setPosition((width - this.btnWidth) / 2, height / 2 + 1.5 * this.btnHeight);
   },
 
   renderMask: function(context) {
